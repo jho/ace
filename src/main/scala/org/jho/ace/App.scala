69d85e411e0269ecf75144c9f9fcd3c7c819b448
Joshua Hollander <jholla14@gmail.com>
2011-03-20 23:51:38 -0600

    Changing the name of the project to ace: Automated Cryptanalysis Engine

---
+++ src/main/scala/org/jho/ace/App.scala
@@ -0,0 +1,10 @@
+package org.jho.autodecrypt
+
+/**
+ * Hello world!
+ *
+ */
+object App {
+  def main(args: Array[String]) {
+  }
+}
---
+++ src/main/scala/org/jho/ace/Vigenere.scala
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2011 Joshua Hollander.
+ */
+
+package org.jho.autodecrypt
+
+class Vigenere(val key:String) {
+  def encrypt(plaintext:String):String = {
+    return null
+  }
+
+  def decrypt(plaintext:String):String = {
+    return null
+  }
+
+  def rotate(s: String, shift:Integer) = {
+  }
+}
