7f394bb8ba495b0c1292b8d57ad940929fe8dccb
Joshua Hollander <jholla14@gmail.com>
2011-03-20 23:34:05 -0600

    Initial checkin.

---
+++ src/test/scala/org/jho/autodecrypt/VigenereTest.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2011 Joshua Hollander.
+ */
+
+package org.jho.autodecrypt
+
+import org.junit._
+import Assert._
+
+class VigenereTest {
+
+    @Before
+    def setUp: Unit = {
+    }
+
+    @After
+    def tearDown: Unit = {
+    }
+
+    @Test
+    def encrypt = {
+      //val v = new Vigenere("blarg")
+      //val result = v.encrypt("this is the plaintext")
+      println('A'.asDigit)
+      println('Z'.asDigit)
+      //assertNotNull(result)
+    }
+
+}
