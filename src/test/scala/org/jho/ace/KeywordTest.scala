aa030ce0eeace44d29e2d02b094983dafa978156
Joshua Hollander <jholla14@gmail.com>
2011-04-10 23:53:01 -0600

    Messing around with generating neighbors of keywords for search

---
+++ src/test/scala/org/jho/ace/KeywordTest.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2011 Joshua Hollander.
+ */
+package org.jho.ace
+
+import org.junit._
+import Assert._
+
+import org.jho.ace.Keyword._
+import org.jho.ace.util.Configuration
+
+class KeywordTest extends Configuration {
+  @Test
+  def neighbors = {
+    var result = "KEYWORD".neighbors
+    println(result)
+
+    println("KEYWORD".permutations)
+  }
+}
